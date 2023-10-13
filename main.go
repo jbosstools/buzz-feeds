688dd85a3e1657d816699f30889bfc4c19544d7c
Stephane Bouchet <sbouchet@redhat.com>
2023-10-13 11:26:59 +0200

    Update main.go

--- main.go
+++ main.go
@@ -90,8 +90,8 @@ func main() {
 	data = strings.Replace(data, "&lt;&lt;", "&amp;lt;&amp;lt;", -1)
 	// replace less than ( not open bracket )
 	data = strings.Replace(data, " &lt; ", " &amp;lt; ", -1)
-	// replace simple &
-	//data = strings.Replace(data, " &amp; ", " &amp;amp; ", -1)
+	// replace #<
+	data = strings.Replace(data, "#&lt;", "#&amp;lt; ", -1)
 	
 
 	f, err := os.OpenFile("rss.xml", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
